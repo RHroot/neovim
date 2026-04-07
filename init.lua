76914d0f2010eeb8d46ddc0187ad2e3a42f103a2
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-04-07 22:21:14 +0530

    automated dev commit

--- init.lua
+++ init.lua
@@ -424,6 +424,20 @@ local servers = {
 		cmd = { "lua-language-server" },
 		filetypes = { "lua" },
 		root_markers = { ".luarc.json", ".git" },
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
 	},
 	clangd = {
 		cmd = { "clangd" },
